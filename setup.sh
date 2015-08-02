9ef835dd22e692380b8317a03c0d9c02e3dc5b56
Ubuntu <ubuntu@ip-172-31-31-130.us-west-2.compute.internal>
2015-08-02 00:41:38 +0000

    ma6174 vim conf

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
-mkdir -p ~/.vim
-mkdir -p ~/.vim/colors
-cp ./monokai.vim ~/.vim/colors/monokai.vim
-mv ./vimrc ~/.vimrc 
+#!/bin/bash
+VIMFOLDER = ./.vim
+VIMRC = ./.vimrc
+
+if [ -d "$VIMFOLDER" ]; then
+    echo "Folder $VIMFOLDER exists. Move original one to ./.vim_old"
+    mv $VIMFOLDER ./.vim_old 
+fi
+
+if [ -f "$VIMRC" ];
+then
+    echo "File $VIMRC exists. Move original one to ./.vim_rc_old"
+    mv $VIMRC ./.vim_rc_old
+fi
+
+# configure vim using ma6174's conf
+echo "START CONFIGURING VIM"
+wget -qO- https://raw.github.com/ma6174/vim/master/setup.sh | sh -x
+
+# Use my own vimrc
+cp ./.vimrc ~/.vimrc 
